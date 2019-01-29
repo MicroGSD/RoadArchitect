f96f0c93e464337dd704d215eca0168461091a6c
FritzsHero <fritzshero@gmail.com>
2019-01-29 09:08:29 +0100

    Changed CommentStyle and Rigidbody name in GSDRigidBody.cs

--- GSDRigidBody.cs
+++ GSDRigidBody.cs
@@ -5,50 +5,72 @@ public class GSDRigidBody : MonoBehaviour
 {
     public float MinCollVelocity = 2f;
     //	bool bIsForcedSleeping = false;
-    Rigidbody RB;
+    Rigidbody rigidbody;            // Formerly RB // FH 29.01.19
     //	bool bIgnoreRB = false;
 
     void Awake()
     {
-        RB = transform.GetComponent<Rigidbody>();
-        if (RB != null)
+        rigidbody = transform.GetComponent<Rigidbody>();
+        if ( rigidbody != null )
         {
-            DestroyImmediate(RB);
+            DestroyImmediate( rigidbody );
         }
     }
 
-    //	void OnCollisionEnter(Collision collision) {
-    //		if(bIgnoreRB || !bIsForcedSleeping){ return; }
-    //		Debug.Log (collision.relativeVelocity.magnitude);
-    //		if(RB != null){
-    //	        if(collision.relativeVelocity.magnitude <= MinCollVelocity){ 
-    //				RB.Sleep();
-    //			}else{
-    //				//RB.isKinematic = false;
-    //				bIsForcedSleeping = false;
-    //				//RB.AddForce(collision.relativeVelocity*collision.relativeVelocity.magnitude*(RB.mass*0.3f));
-    //			}
-    //		}
-    //	}
-    //	
-    //	void OnCollisionExit(Collision collisionInfo) {
-    //		if(bIgnoreRB || !bIsForcedSleeping){ return; }
-    //       	if(bIsForcedSleeping && RB != null){
-    //			RB.Sleep();	
-    //		}
-    //    }
-    //	
-    //	float TimerMax = 0.1f;
-    //	float TimerNow = 0f;
-    //	void Update(){
-    //		if(bIsForcedSleeping){
-    //			TimerNow += Time.deltaTime;
-    //			if(TimerNow > TimerMax){
-    //				if(RB != null && !RB.IsSleeping()){
-    //					RB.Sleep();	
-    //				}
-    //				TimerNow = 0f;
-    //			}
-    //		}
-    //	}
+
+    /*
+    void OnCollisionEnter(Collision collision)
+    {
+        if ( bIgnoreRB || !bIsForcedSleeping )
+        {
+            return;
+        }
+        Debug.Log( collision.relativeVelocity.magnitude );
+        if ( rigidbody != null )
+        {
+            if ( collision.relativeVelocity.magnitude <= MinCollVelocity )
+            {
+                rigidbody.Sleep();
+            }
+            else
+            {
+                //RB.isKinematic = false;
+                bIsForcedSleeping = false;
+                //RB.AddForce(collision.relativeVelocity*collision.relativeVelocity.magnitude*(RB.mass*0.3f));
+            }
+        }
+    }
+
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        if ( bIgnoreRB || !bIsForcedSleeping )
+        { return; }
+        if ( bIsForcedSleeping && rigidbody != null )
+        {
+            rigidbody.Sleep();
+        }
+    }
+
+
+    float TimerMax = 0.1f;
+    float TimerNow = 0f;
+
+
+    void Update()
+    {
+        if ( bIsForcedSleeping )
+        {
+            TimerNow += Time.deltaTime;
+            if ( TimerNow > TimerMax )
+            {
+                if ( rigidbody != null && !rigidbody.IsSleeping() )
+                {
+                    rigidbody.Sleep();
+                }
+                TimerNow = 0f;
+            }
+        }
+    }
+    */
 }
